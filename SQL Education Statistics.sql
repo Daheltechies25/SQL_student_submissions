b0c5d008fb18051745d19de0ea43dcc100881b2d
Lois Chidumebi Ellion-Tanze <chidumebilois@gmail.com>
2026-01-17 19:08:20 +0000

    Add files via upload

---
+++ SQL Education Statistics.sql
@@ -0,0 +1,8 @@
+Create database education_db;
+Use education_db;
+Show databases;
+Use education_db;
+Use education_db;
+Show Tables;
+Select * From education_db limit 10;
+Select * From education limit 10;
